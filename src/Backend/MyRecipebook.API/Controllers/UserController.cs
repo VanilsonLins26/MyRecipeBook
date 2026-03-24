feaa4d1e32659438238f7c8d790f4ccef92ac9a7
vanilson <vanilson.liins@gmail.com>
2026-03-23 21:01:29 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/MyRecipebook.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipebook.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Register(RequestRegisterUserJson request) 
+    {
+        return Created();
+    }
+}
--- src/Backend/MyRecipebook.API/Program.cs
+++ src/Backend/MyRecipebook.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,46 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    //var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    //c.IncludeXmlComments(xmlPath);
+
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiRecipeBook", Version = "v1" });
+
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+
+                AuthorizationUrl = new Uri("https://localhost:5001/connect/authorize"),
+                TokenUrl = new Uri("https://localhost:5001/connect/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    { "iorder_api", "Acesso à API IOrder" },
+                    { "openid", "OpenID" },
+                    { "profile", "Profile" }
+                }
+            }
+        }
+    });
+
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "API Meu livro de receitas"));
 }
 
 app.UseHttpsRedirection();
